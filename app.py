e9069b9647065d7e3325676321e687f7d0b93a44
yfHHHE <74725781+yfHHHE@users.noreply.github.com>
2024-12-09 12:26:33 +0000

    Add in Gen AI

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, request
 import textblob
-
+import google.generativeai as gnai
 
 app = Flask("__name__")
 
@@ -23,5 +23,19 @@ def SA_result():
     q = request.form.get("q")
     r = textblob.TextBlob(q).sentiment
     return (render_template("sa_result.html",r=r))
+
+@app.route("/AI",methods=['GET','POST'])
+def AI():
+    return (render_template("ai.html"))
+
+@app.route("/AI_result",methods=['GET','POST'])
+def AI_result():
+    q = request.form.get("q")
+    api = 'AIzaSyDMGJl5TE73oqQ7UWOqaE4Z1TPpHNfjjPM'
+    gnai.configure(api_key=api)
+    model = gnai.GenerativeModel("gemini-1.5-flash")
+    r = model.generate_content(q)
+    r = r.candidates[0].content.parts[0].text
+    return (render_template("ai_result.html",r=r))
 if __name__ == '__main__':
     app.run()
